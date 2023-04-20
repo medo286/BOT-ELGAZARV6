4fee0b55f0c2cd70746052d7cf22437a46db2bc8
gon frecks <112989978+Gonfreks@users.noreply.github.com>
2023-04-20 04:24:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['201146121794'] //ur owner number
+global.ownername = "العم طوماس☃️" //ur owner name
+global.ytname = "YT: ELZA3EM" //ur yt chanel name
+global.socialm = "GitHub: ELZA3EM" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "201146121794" //ur number
+global.premium = ['201146121794'] //ur premium number
+global.botname = '☃️𝑬𝑳𝒁𝑨3𝑬𝑴 𝑮𝑶𝑵☃️' //ur bot name
+global.linkz = "https://youtube.com/@MR_ThomasFF" //your theme url which will be displayed on whatsapp
+global.websitex = "https://telegra.ph/file/25ac27774c8a0420e2649.jpg" //ur website to be displayed
+global.botscript = 'https://youtube.com/@MR_ThomasFF' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣حقوق البوت غون⍣" //ur sticker watermark packname
+global.author = "⍣لا للسرقه! 🔪👻⍣" //ur sticker watermark author
+global.wm = "☃️𝑬𝑳𝒁𝑨3𝑬𝑴 𝑮𝑶𝑵☃️." //ur watermark
 
 // Other
 global.sessionName = 'session'
